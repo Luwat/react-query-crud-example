1ab301926e0550bee48549028f7ec011f30da0d3
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-04-14 14:07:09 +0100

    Implement edit todo

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddTodo from "./components/AddTodo.tsx";
+import EditTodo from "./components/EditTodo.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
   {
     path: "/add-todo",
     element: <AddTodo />
+  },
+  {
+    path: "/edit-todo/:id",
+    element: <EditTodo />,
   },
   {
     path: "*",
